2b4a80414c1a48c88fbe2dcad75d08021641050d
andre-0303 <bandeiradev.ti@gmail.com>
2025-03-26 19:41:25 -0300

    Atividade sobre variáveis

---
+++ meu_nome.js
@@ -0,0 +1,8 @@
+function pessoa() {
+  let nome = "André Bandeira";
+  let idade = 17;
+  const cidade = "Croatá da Serra";
+  console.log(`Nome: ${nome}, Idade: ${idade}, Cidade: ${cidade}`);
+}
+
+pessoa();
