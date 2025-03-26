effb1df27a591b45fd4ab203e2094c5542324352
andre-0303 <bandeiradev.ti@gmail.com>
2025-03-26 19:35:48 -0300

    Variáveis em js

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('Curso de JavaScript');
+
+//variáveis
+/*
+var - maneira tradicional (não recomendado);
+let - variável que pode ser alterada (recomendado - uso moderno);
+const - variável que não pode ser alterada (recomendado - uso moderno);
+*/
+
+let idade = 34;
+const cpf = '123.456.789-00';
+const cidade = 'Croatá da Serra';
+
+console.log(idade);
+console.log(cpf);
+console.log(cidade);
+
